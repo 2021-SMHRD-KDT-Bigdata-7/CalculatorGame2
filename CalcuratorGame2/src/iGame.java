4df9964c521a374e7fef4fda3e62750f4299ef42
smhrd <smhrd@DESKTOP-70QF4QM>
2021-10-13 17:40:40 +0900

    인터페이스

--- CalcuratorGame2/src/iGame.java
+++ CalcuratorGame2/src/iGame.java
@@ -1,21 +1,13 @@
 
 public interface iGame {
 	
-	//랜덤문제생성, 더하기문제출제, 빼기문제출제, 곱하기문제출제, 나누기문제출제
+	//랜덤문제생성, 
 	
 	    public void makeRandom();
-	
-		public void PlusGame();
-		
-		public void MinusGame();
-		
-		public void MulGame();
-		
-		public void DivGame();
 		
 		public String getQuizMsg();
 		
-		public boolean checkAnswer(int answer);
+		public boolean checkAnswer(double answer);
 	
 	
 
