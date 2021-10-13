80a2fd22aad27790008bf34451357f84d989fb55
smhrd <smhrd@DESKTOP-FQCRA9H>
2021-10-13 17:53:33 +0900

    # WARNING: head commit changed in the meantime
    나누기

---
+++ CalcuratorGame2/src/DivGame.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class DivGame implements iGame {
+	private Random ran;
+	private int num1;
+	private int num2;
+
+	@Override
+	public void makeRandom() {
+		ran = new Random();
+		num1 = ran.nextInt(100) + 1;
+		num2 = ran.nextInt(100) + 1;
+		
+	}
+
+	@Override
+	public String getQuizMsg() {
+		return num1 + "/" + num2 + "=";
+	}
+
+	@Override
+	public boolean checkAnswer(double answer) {
+		return (answer==(num1/num2)) ? true : false;
+	}
+
+}
